46aed0b44a8d93c57a2d38c76b15b46aa2791f0b
Evrim P <evrimfeyyaz@gmail.com>
2018-07-15 16:05:28 +0300

    Add navigational links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def add_link_title_for(category_obj)
+    "Add #{category_obj.class.name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'categories#index'
 
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index, :show] do
+    resources :items, only: [:new]
+  end
+
   resources :items, only: [:show]
 end
