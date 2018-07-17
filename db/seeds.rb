1b3760ee0a9c745e9e34c7b1fd483f8de866a214
Evrim P <evrimfeyyaz@gmail.com>
2018-07-17 09:48:32 +0300

    Update the seeds file based on recent changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+# Create categories.
 15.times do
   category_title       = Faker::Fallout.faction
   category_description = Faker::Fallout.quote
 
   category = Category.create!(title: category_title, description: category_description)
 
+  # Add resources to the category.
   10.times do
     resource_title       = Faker::Fallout.character
     resource_description = Faker::Fallout.quote
@@ -19,21 +21,27 @@
                                 youtube:     links_url,
                                 facebook:    links_url)
 
+    # Add lists to the resource.
     3.times do
       list_title = Faker::Fallout.faction
 
       list = List.create!(title:    list_title,
                           resource: resource)
 
+      # Add a regular resource to the list.
+      resource = Resource.order("RANDOM()").first
+      ListItem.create(listable: resource, list: list)
+
+      # Add external resources to the list.
       3.times do
         external_resource_title       = Faker::Fallout.character
         external_resource_description = Faker::Fallout.quote
         external_resource_url         = 'http://www.example.com'
 
-        ExternalResource.create!(title:       external_resource_title,
+        external_resource = ExternalResource.create!(title:       external_resource_title,
                                  description: external_resource_description,
-                                 url:         external_resource_url,
-                                 list:        list)
+                                 url:         external_resource_url)
+        ListItem.create(listable: external_resource, list: list)
       end
     end
   end
