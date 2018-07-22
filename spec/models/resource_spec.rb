a53a45c8815b2d5609e10ab6a7c5a181801ea31b
Evrim P <evrimfeyyaz@gmail.com>
2018-07-22 13:39:29 +0300

    Add sluggable specs to the resource model

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Resource do
+  it_behaves_like 'a sluggable'
+
   it { should have_and_belong_to_many(:categories) }
   it { should have_many(:lists).dependent(:destroy) }
   it { should have_many(:list_items).dependent(:destroy) }
