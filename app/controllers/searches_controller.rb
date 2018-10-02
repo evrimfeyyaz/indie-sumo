9a86af1c189d74a15794c812b2fcc73a446bdda6
Evrim P <evrimfeyyaz@gmail.com>
2018-10-02 12:00:54 +0300

    Add pagination to search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,11 +7,11 @@ class SearchesController < ApplicationController
 
     def build_search_results
       @search_results ||= Resource.search(params[:query],
-                                          highlight: { tag: '<mark>' },
-                                          load:      true,
                                           fields:    [{ 'title^5': :word_start },
                                                       { 'description^4': :word_start },
                                                       { 'links^3': :word_middle },
-                                                      { 'categories^2': :word_start }])
+                                                      { 'categories^2': :word_start }],
+                                          page: params[:page],
+                                          per_page: Kaminari.config.default_per_page)
     end
 end
