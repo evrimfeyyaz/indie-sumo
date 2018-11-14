90acde6c13c28f32600f065c807378f5a487bb3a
Evrim P <evrimfeyyaz@gmail.com>
2018-11-14 16:04:42 +0300

    Hide resource icon div when there is no icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-  # The original #link_to_if helper does not print out the
-  # given block when the condition succeeds, and also does
-  # not allow a class option. This method prints out a link
-  # if the condition succeeds, and a div if it fails, with
-  # the given class in both cases.
-  def link_to_or_block(condition, url, success_class: nil, fail_class: nil, &block)
-    if condition
-      return link_to(url, class: success_class, &block)
-    end
-
-    content_tag('div', class: fail_class, &block)
-  end
-
   # Properly links to resources (which have their own pages) and
   # external resources (which point to external URLs).
   def link_to_resource(resource, html_options = nil, &block)
