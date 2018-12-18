d72463dda017aeff72f0f73e7968c11eb2427426
Evrim P <evrimfeyyaz@gmail.com>
2018-12-18 11:19:09 +0300

    Try adding static meta tags to see if the Facebook scraper issue persists

--- app/assets/javascripts/linkedin_share.js
+++ app/assets/javascripts/linkedin_share.js
@@ -1,9 +1,9 @@
-// $(function () {
-//   function loadLinkedIn() {
-//     delete IN
-//     $.getScript("//platform.linkedin.com/in.js")
-//   }
-//
-//   loadLinkedIn();
-//   $(document).on('turbolinks:load', loadLinkedIn)
-// })
+$(function () {
+  function loadLinkedIn() {
+    delete IN
+    $.getScript("//platform.linkedin.com/in.js")
+  }
+
+  loadLinkedIn();
+  $(document).on('turbolinks:load', loadLinkedIn)
+})
