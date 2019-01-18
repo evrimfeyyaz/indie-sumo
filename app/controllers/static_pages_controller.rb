b9c66e47e9f83672650f8cf3fb8efd1564d0233e
Evrim P <evrimfeyyaz@gmail.com>
2019-01-18 18:58:34 +0300

    Remove the privacy policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,6 @@ class StaticPagesController < ApplicationController
   def newsletter
   end
 
-  def privacy_policy
-  end
-
   def loader_io_verification_staging
     render plain: 'loaderio-af1f76c262397ab753e3f2ef4dc64834'
   end
