fdb03382dece1ec2c16931a1c73e3e987f1d8fb6
Evrim P <evrimfeyyaz@gmail.com>
2019-01-18 19:01:38 +0300

    Remove share buttons

--- app/assets/javascripts/linkedin_share.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  function loadLinkedIn() {
-    delete IN
-    $.getScript("//platform.linkedin.com/in.js")
-  }
-
-  loadLinkedIn();
-  $(document).on('turbolinks:load', loadLinkedIn)
-})
