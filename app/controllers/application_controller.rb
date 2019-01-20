af252b2e53d7ff09dafdd239d60d099788fa07fe
Evrim P <evrimfeyyaz@gmail.com>
2019-01-20 13:06:34 +0300

    Bust ETags using the app version on Heroku in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  etag { heroku_version }
+
+  private
+
+  def heroku_version
+    ENV['HEROKU_RELEASE_VERSION'] if Rails.env == 'production'
+  end
 end
