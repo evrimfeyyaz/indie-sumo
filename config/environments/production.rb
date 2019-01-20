609baa69ef8007e05097309ead5a119a8b355f51
Evrim P <evrimfeyyaz@gmail.com>
2019-01-20 15:17:47 +0300

    Server error pages from the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Serve error pages from the Rails app.
+  config.exceptions_app = self.routes
 end
